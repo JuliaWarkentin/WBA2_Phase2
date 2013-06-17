c03551ddf81db24c0be717fb114a3577a2a21b9c
Simon Klinge <santakrass@web.de>
2013-06-17 16:01:03 +0200

    +XMPPData

---
+++ WBA2_Phase2/src/xmpp/XMPPData.java
@@ -0,0 +1,13 @@
+package xmpp;
+/** 
+ * Konstanten zu unserem XMPP-Server
+ * 
+ * @author Simon Klinge
+ * @author Julia Warkentin
+ *
+ */
+public class XMPPData {
+	public static final String host = "localhost";
+	public static final String expirationNodeID = "expiration";
+	public static final String stolenNodeID = "stolen";
+}
